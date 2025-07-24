a530c55789fdc15830c5b76e9d9590ba058dfbec
sp <71633038+salonynhp@users.noreply.github.com>
2025-07-24 11:34:15 +0530

    Update index.js

--- MyFlutterProjects/backend/src/index.js
+++ MyFlutterProjects/backend/src/index.js
@@ -5,7 +5,7 @@ const userRoutes = require("../API/UserController.js"); // 👈 Imported control
 const cors = require('cors'); 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
